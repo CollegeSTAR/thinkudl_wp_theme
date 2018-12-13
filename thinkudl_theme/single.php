72566980c66aa0e00975d5bf465a318a4718573b
Joel Tanner Jones <jonesjoe@ecu.edu>
2018-12-13 11:34:53 -0500

    Create single.php

---
+++ thinkudl_theme/single.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+  
+  <?php while ( have_posts() ) : the_post(); ?>
+    <div id="episode_wrapper">
+      <h1><?php the_title(); ?></h1> 
+      <?php the_content(); ?>
+    </div>
+  <?php endwhile; ?>
+
+<?php get_footer(); ?>
